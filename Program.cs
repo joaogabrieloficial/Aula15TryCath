ae17ca2605c6ea48386b7e3c55a54ba10ef60203
unknown <joaogabriel301199@gmail.com>
2020-06-11 10:36:13 -0300

    Projeto Finalizado

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Aula15TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Permissao app = new Permissao();
+           app.Autorizar();
+        }
+    }
+}
